c44fc524746baff68e239ec11299bf597445862b
khangit96 <khangit69@gmail.com>
2018-03-26 22:09:58 +0700

    fjfdj

--- ServerPython/download.py
+++ ServerPython/download.py
@@ -1,6 +1,8 @@
-import urllib.request, urllib.parse, urllib.error 
+import urllib 
 import pyrebase
 import os
+import threading
+from playsound import playsound
 
 config = {
   "apiKey": "uTP6tlP930oA1s9zuwGIZvrz1ef8ZjVLegROgNN0",
@@ -12,24 +14,23 @@ check=False
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
-def stream_handler(message):
-    # ghiAmList = db.child("BeTapKeChuyen/GhiAm").get()
+urllib.urlretrieve('https://firebasestorage.googleapis.com/v0/b/smarthome-5d11a.appspot.com/o/G6CR37O1JTRIBIWE3T.mp3?alt=media&token=e37b635d-12c5-4771-8a49-b30a30e8d01d','khang.mp3')
+#Play voice
+def playVoice(arg,arg2):
+     os.system("mpg123 "+arg)
 
-    # # length=len(ghiAmList.each())
-    # # print(str(length))
-    # # lastItem=ghiAmList.each()[length-1].val()
-    # # print(lastItem['fileName'])
-    # for ghiAm in ghiAmList.each():
-    #     print(ghiAm.val()["fileName"])
+def stream_handler(message):
     global check
     if check:
         fileUrl =message["data"]["urlDownload"]
-        urllib.request.urlretrieve(fileUrl,message["data"]["fileName"])
+        fileName=message["data"]["fileName"]
+        urllib.urlretrieve(fileUrl,fileName)
+        # thread = threading.Thread(target=playVoice, args=(message["data"]["fileName"],'fkf'))
+        # thread.start()
+        playsound(fileName)
         print('done')
     else:
       print('listening')
       check= True
   
-
-    
 my_stream = db.child("BeTapKeChuyen/LastGhiAm").stream(stream_handler)
