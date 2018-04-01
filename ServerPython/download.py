ceaf66590d0bfd358fef99527afe1e1fccffca82
tdmuteam <tdmuteam2016@gmail.com>
2018-04-01 06:03:23 +0000

    dfjgfjgf

--- ServerPython/download.py
+++ ServerPython/download.py
@@ -14,7 +14,6 @@ check=False
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
-urllib.urlretrieve('https://firebasestorage.googleapis.com/v0/b/smarthome-5d11a.appspot.com/o/G6CR37O1JTRIBIWE3T.mp3?alt=media&token=e37b635d-12c5-4771-8a49-b30a30e8d01d','khang.mp3')
 #Play voice
 def playVoice(arg,arg2):
      os.system("mpg123 "+arg)
@@ -25,9 +24,8 @@ def stream_handler(message):
         fileUrl =message["data"]["urlDownload"]
         fileName=message["data"]["fileName"]
         urllib.urlretrieve(fileUrl,fileName)
-        # thread = threading.Thread(target=playVoice, args=(message["data"]["fileName"],'fkf'))
-        # thread.start()
-        playsound(fileName)
+        os.system('omxplayer -o both '+fileName)
+
         print('done')
     else:
       print('listening')
